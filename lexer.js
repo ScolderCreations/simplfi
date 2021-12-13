2e82f31246a4d21b471699eb47948a27b2a9b424
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-12-13 13:47:54 -0800

    Create lexer.js

---
+++ lexer.js
@@ -0,0 +1,13 @@
+const tokenOf = {
+  "+": 'PLUS',
+  "-": 'DASH',
+  ".": 'PERIOD',
+  "'": 'SMALLQUOTE',
+  '"': 'BIGQUOTE',
+  "*": 'ASTERISK',
+  "!": 'EXCLAIM',
+  "?": 'QUESTION',
+  "%": 'PERCENT',
+  '@': 'AT',
+  '(': 'LEFTPARENT',
+  ')': 'RIGHTPARENT'
