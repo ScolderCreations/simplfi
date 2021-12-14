a2b83d3b6b937c8b467614a668efdc3f1a41cd2b
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-12-14 00:31:49 +0000

    type detection

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const fs = require('fs');
 const prompt = require('prompt-sync')();
 const inputFileRaw = new String(fs.readFileSync(prompt()));
 
-var inputString = inputFileRaw.split('\n')
-
-var inputString = new Object(inputString.forEach(function(a){return a.split('\s')}))
+var inputString = inputFileRaw.split('\n');
+const listVals = [];
+var getLines = (function(){inputString.forEach(function(a){listVals.push(a.split(' '))}); return listVals})
+getLines();
+for (line in listVals) { // targets one line at a time
+  for (token in line) {
+    var tokentype;
+    if (lexer.tokenOf[token]) {
+      tokentype = lexer.tokenOf[token];
+    } 
+    else if (!isNaN(new Number(token))) 
+    {tokentype = "NUMBER"}
+    else if (typeof token == 'string')
+    {tokentype =  "STRING"}
+    }
+  }
+}
