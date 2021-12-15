bcf4f1cdb2eb3452614ad65151469b74816d59dc
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-12-15 21:41:28 +0000

    actually works kinda

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-const lexer = require('./tokens.js');
+const tokens = require('./tokens.js');
 const fs = require('fs');
 const prompt = require('prompt-sync')();
-const inputFileRaw = new String(fs.readFileSync(prompt()));
-
+const getFilePath = function(){const a = prompt(); if (a.endsWith(".lfi")) {return a} else {return a + ".lfi"}}
+const inputFileRaw = new String(fs.readFileSync(getFilePath()));
+var outputJS = new String()
+console.log("")
 var inputString = inputFileRaw.split('\n');
 const listVals = [];
 var getLines = (function(){inputString.forEach(function(a){listVals.push(a.split(' '))}); return listVals})
 getLines();
+function convertToToken(token, input) {
+  var output;
+  if (tokens.tokenOf[token]) {
+    if (tokens.tokenOf[token].includes('•')) { 
+      output = input + tokens.tokenOf[token] + " "
+    } else {
+      output = input + tokens.tokenOf[token] + " "
+    }} else {
+      output = input + String(token) + " "
+  }
+  return output
+}
+for (line of listVals) {
+  var ti="";
+  var lineind=new Number(0 - 1);
+  for (token of line) {
+    var outputJS, ti, lineind/*, tokenAppend*/
+    lineind++
+    if (!ti) {
+    if (tokens.tokenOf[token]) {
+      if (tokens.tokenOf[token].includes('•')) { 
+        ti = 's'
+        outputJS = outputJS + tokens.tokenOf[token].replace("•", line[lineind + 1]) + " "
+      } else {
+        outputJS = outputJS + tokens.tokenOf[token] + " "
+      }
+    } else {
+      outputJS = outputJS + String(token) + " "
+    } 
+    } else {ti=''}
+  } outputJS+="\n"
+}
